17de6dd3380056f26877c7272d4423b00218c072
Ignacio Aguiar <nacho3562@hotmail.com>
2021-06-18 18:10:19 -0300

    Subida de alumno

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Cedula = new System.Windows.Forms.TextBox();
+            this.Nombre = new System.Windows.Forms.TextBox();
+            this.Apellido = new System.Windows.Forms.TextBox();
+            this.Clave = new System.Windows.Forms.TextBox();
+            this.ClaveComprobación = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Cedula
+            // 
+            this.Cedula.Location = new System.Drawing.Point(12, 26);
+            this.Cedula.Name = "Cedula";
+            this.Cedula.Size = new System.Drawing.Size(100, 20);
+            this.Cedula.TabIndex = 1;
+            // 
+            // Nombre
+            // 
+            this.Nombre.Location = new System.Drawing.Point(12, 52);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(100, 20);
+            this.Nombre.TabIndex = 2;
+            // 
+            // Apellido
+            // 
+            this.Apellido.Location = new System.Drawing.Point(12, 78);
+            this.Apellido.Name = "Apellido";
+            this.Apellido.Size = new System.Drawing.Size(100, 20);
+            this.Apellido.TabIndex = 3;
+            // 
+            // Clave
+            // 
+            this.Clave.Location = new System.Drawing.Point(12, 104);
+            this.Clave.Name = "Clave";
+            this.Clave.Size = new System.Drawing.Size(100, 20);
+            this.Clave.TabIndex = 4;
+            // 
+            // ClaveComprobación
+            // 
+            this.ClaveComprobación.Location = new System.Drawing.Point(12, 130);
+            this.ClaveComprobación.Name = "ClaveComprobación";
+            this.ClaveComprobación.Size = new System.Drawing.Size(100, 20);
+            this.ClaveComprobación.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClaveComprobación);
+            this.Controls.Add(this.Clave);
+            this.Controls.Add(this.Apellido);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.Cedula);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Cedula;
+        private System.Windows.Forms.TextBox Nombre;
+        private System.Windows.Forms.TextBox Apellido;
+        private System.Windows.Forms.TextBox Clave;
+        private System.Windows.Forms.TextBox ClaveComprobación;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MySql.Data.MySqlClient;
 namespace PruebasGiT
 {
     public partial class Form1 : Form
@@ -15,6 +15,35 @@ namespace PruebasGiT
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexion = new MySqlConnection(
+                "server=192.168.10.145;" +
+                "userid=root;" +
+                "password=1234;" +
+                "database=test"
+                );
+
+            conexion.Open();
+
+            MySqlCommand Ingreso = new MySqlCommand();
+            Ingreso.Connection = conexion;
+            Ingreso.CommandText = "INSERT INTO alumno(cedula,nombre,apellido,clave) VALUE(@cedula,@nombre,@apellido,@clave)";
+
+            Ingreso.Parameters.AddWithValue("@cedula", Cedula.Text);
+            Ingreso.Parameters.AddWithValue("@nombre", Nombre.Text);
+            Ingreso.Parameters.AddWithValue("@apellido", Apellido.Text);
+            Ingreso.Parameters.AddWithValue("@clave", Clave.Text);
+
+            Ingreso.Prepare();
+
+            Ingreso.ExecuteNonQuery();
+
+            conexion.Close();
+
+            
         }
     }
 }
